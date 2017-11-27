3535550de76d8b7d713ae82a167472e6540f570e
SBelondrade <31722469+SBelondrade@users.noreply.github.com>
2017-11-27 11:16:31 +0100

    producteur

--- controleur/controleurResponsable.php
+++ controleur/controleurResponsable.php
@@ -1,4 +1,20 @@
 <?php
+// -->modif compte
+
+
+// -->Enregistrer un nouveau producteur.
+
+
+// -->Créer une nouvelle vente.
+
+
+// -->Ouvrir/fermer l'autorisation de saisie des producteurs pour une nouvelle vente.
+
+
+// -->Ouvrir/fermer l'autorisation la saisie des commandes pour une nouvelle vente.
+
+
+// ->Ajouter une nouvelle catégorie de produits.
 
 
 require_once "vue/vueResponsable.php";
